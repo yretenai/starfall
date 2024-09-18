6858dbac39685a3c9ac1b4a47252093c504f2f13
Ada <yretenai@gmail.com>
2024-09-18 05:41:29 +0100

    add docc plugin

--- Package.swift
+++ Package.swift
@@ -7,6 +7,7 @@ let package = Package(
 	products: [],
 	dependencies: [
 		.package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+		.package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0"),
 	],
 	targets: [
 		.executableTarget(
